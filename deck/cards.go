6e8e7680452dfad17485242a957b016d3bfcac3a
Magus <32424385+sprkbrn@users.noreply.github.com>
2019-05-02 00:46:18 -0400

    refactoring - minor changes

--- deck/cards.go
+++ deck/cards.go
@@ -1,23 +1,20 @@
 package deck
 
-//card represents a playing card
-type card struct {
-	Face  string //the face value of the card
-	Court suite  //the group the card is in
-}
-
 //suite represents the group a card is in
 type suite struct {
 	Name   string //the name of the suite
 	Symbol string //symbol of representation
 }
 
+//card represents a playing card
+type card struct {
+	Face  string //the face value of the card
+	Court suite  //the group the card is in
+}
+
 //all possible faces a card can have
 var cardFaces = []string{"A", "1", "2", "3", "4", "5",
 	"6", "7", "8", "9", "10", "J", "Q", "K"}
-
-//CardGroups is a collection of our suites
-var CardGroups = []suite{Diamonds, Spades, Hearts, Clubs}
 
 //One of the four prominent suites in playing cards
 var (
@@ -38,6 +35,9 @@ var (
 		"♣",
 	}
 )
+
+//CardGroups is a collection of our suites
+var CardGroups = []suite{Diamonds, Spades, Hearts, Clubs}
 
 //DeckOfCards represents all possible playing cards
 var DeckOfCards = func() []card {
